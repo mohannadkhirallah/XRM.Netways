eb13e663a0d810ceff0fdd89c52e2555e0e6a952
Mohannad Kheralla <mkheralla@Netways.com>
2023-10-02 12:58:44 +0300

    Ninject on concole application

---
+++ XRM.Core/Utlities/Infrastructure/Ninject/NinjectConsoleFactory.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XRM.Core.Utlities.Infrastructure.Ninject
+{
+    public class NinjectConsoleFactory
+    {
+        IKernel kernel;
+        public NinjectConsoleFactory(params INinjectModule[] modules)
+        {
+            kernel = new StandardKernel(modules);
+        }
+        public IKernel GetKernel()
+        {
+            return kernel?? throw new InvalidOperationException("No Kernel Is empty");
+        }
+
+    }
+}
--- XRM.Netways.ConsoleUI/Program.cs
+++ XRM.Netways.ConsoleUI/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Ninject;
+using System;
+using System.Reflection;
+using XRM.Core.Utlities.Infrastructure.Ninject;
 using XRM.Netways.Business.Abstract;
 using XRM.Netways.Business.Concrete;
+using XRM.Netways.Business.DependencyResolver.Ninject;
 using XRM.Netways.DataAccess.Concrete.EntityFramework;
 
 namespace XRM.Netways.ConsoleUI
@@ -9,7 +13,14 @@ namespace XRM.Netways.ConsoleUI
     {
         static void Main(string[] args)
         {
-            IProductService service = new ProductManager(new EfProductDal());
+           // IProductService service = new ProductManager(new EfProductDal());
+
+            var factory = new NinjectConsoleFactory(new BusinessModule());
+
+            var kernel = factory.GetKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            IProductService service = kernel.Get<IProductService>();
+
 
             foreach (var item in service.GetAll())
             {
